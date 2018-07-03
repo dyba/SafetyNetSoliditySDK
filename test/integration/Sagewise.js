ce9501daeebd76b40d2aee95506854710fa98a4e
Daniel Dyba <daniel.dyba@gmail.com>
2018-07-02 17:40:33 -0700

    Add skeletal structure for tests

---
+++ test/integration/Sagewise.js
@@ -0,0 +1,1 @@
+var Sagewise = artifacts.require("Sagewise");
---
+++ test/unit/TestSagewise.sol
@@ -0,0 +1,6 @@
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../../contracts/Sagewise.sol";
+
+contract TestSagewise {
+}
